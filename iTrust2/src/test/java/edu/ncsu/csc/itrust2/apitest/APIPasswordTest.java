4a3393694919acf72091bdf8a79f03bc6c79d462
Allen Thomas <Allen@Allens-MacBook-Pro-3.local>
2018-02-14 20:10:59 -0500

    added tests for password change functionality

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/apitest/APIPasswordTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/apitest/APIPasswordTest.java
@@ -86,13 +86,22 @@ public class APIPasswordTest {
         personnel.setState( State.NC.toString() );
         personnel.setZip( "27514" );
         mvc.perform( post( "/api/v1/personnel" ).contentType( MediaType.APPLICATION_JSON )
-                .content( TestUtils.asJsonString( personnel ) ) );
+                .content( TestUtils.asJsonString( personnel ) ) ).andExpect( status().isOk() );
 
         assertTrue( pe.matches( "123456", user.getPassword() ) );
         changePassword( user, "123456", "654321" );
         user = User.getByName( "patientPW" ); // reload so changes are visible
         assertFalse( pe.matches( "123456", user.getPassword() ) );
         assertTrue( pe.matches( "654321", user.getPassword() ) );
+
+        // invalid password because the new password doesnt match with repeated
+        // pass
+        final PasswordChangeForm form = new PasswordChangeForm();
+        form.setCurrentPassword( user.getPassword() );
+        form.setNewPassword( "1234567" );
+        form.setNewPassword2( "123457" );
+        mvc.perform( post( "/api/v1/changePassword" ).contentType( MediaType.APPLICATION_JSON )
+                .content( TestUtils.asJsonString( form ) ) ).andExpect( status().isBadRequest() );
 
         final Personnel p = Personnel.getByName( user );
         p.delete();
@@ -125,4 +134,42 @@ public class APIPasswordTest {
 
     }
 
+    // /**
+    // * This tests that api for resetting a password
+    // *
+    // * @throws Exception
+    // */
+    // @WithMockUser ( username = "patientPW2", roles = { "USER", "ADMIN" } )
+    // @Test
+    // public void testPasswordReset () throws Exception {
+    // final UserForm patient = new UserForm( "patientPW2", "123456",
+    // Role.ROLE_PATIENT, 1 );
+    // User user = new User( patient );
+    // user.save();
+    //
+    // user = User.getByName( "patientPW2" ); // ensure they exist
+    // final PersonnelForm personnel = new PersonnelForm();
+    // personnel.setAddress1( "1 Test Street" );
+    // personnel.setAddress2( "Address Part 2" );
+    // personnel.setCity( "Prag" );
+    // personnel.setEmail( "csc326s100x@gmail.com" );
+    // personnel.setFirstName( "Test" );
+    // personnel.setLastName( "HCP" );
+    // personnel.setPhone( "123-456-7890" );
+    // personnel.setSelf( user.getUsername() );
+    // personnel.setState( State.NC.toString() );
+    // personnel.setZip( "27514" );
+    // mvc.perform( post( "/api/v1/personnel" ).contentType(
+    // MediaType.APPLICATION_JSON )
+    // .content( TestUtils.asJsonString( personnel ) ) ).andExpect(
+    // status().isOk() );
+    // mvc.perform( post( "/api/v1/requestPasswordReset" ).contentType(
+    // MediaType.APPLICATION_JSON )
+    // .content( personnel.getSelf() ) ).andExpect( status().isOk() );
+    //
+    // final Personnel p = Personnel.getByName( user );
+    // p.delete();
+    // user.delete();
+    //
+    // }
 }
